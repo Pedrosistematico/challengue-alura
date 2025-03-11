77aaf4900db3a4273c6eee56f9d19d7e2995a56a
Pedro Lluen <lluensistemas@gmail.com>
2025-03-11 17:29:06 -0500

    Actualizando el app

--- app.js
+++ app.js
@@ -1,4 +1,19 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 
-let amigos = [];
+let totalAmigos = [];
+
+function agregarAmigo(){
+    let amigos = document.getElementById('amigo').value; 
+
+    if( amigos == ""){
+        alert("Por favor, inserte un nombre.");
+    }else{
+        totalAmigos.push(amigos);
+        console.log(totalAmigos);
+
+        document.getElementById('amigo').value = "";
+        document.getElementById('listaAmigos').innerText = `Amigos: \n ${totalAmigos.join("\n ")}`;
+    }
+}
+
 
